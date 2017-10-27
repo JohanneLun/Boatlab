811bcd70fc4ae3f338a5b5d51e3f10f0cc431f22
Karoline Amalie Haugland Halvorsen <kahalvor@ntnu.no>
2017-10-27 17:11:08 +0200

    Part 2 ferdig

--- Boatfiles/Part 2/p2_init.m
+++ Boatfiles/Part 2/p2_init.m
@@ -16,4 +16,23 @@ load('wave.mat');
 Fs = 10;
 window_size = 4096;
 
-[pxx,f] = pwelch(psi_w(2,:),noverlap, nfft, window_size,Fs);
+[pxx,f] = pwelch(psi_w(2,:)*(pi/180), window_size,[],[],Fs);
+omega= 2*pi*f; %rad/s]                  
+pxx= pxx./(2*pi); %[s/rad]
+
+figure
+plot(omega, pxx);
+hold on;
+%w_0 was identifyd by the plot 
+w_0 = 0.7823; 
+
+%Calculating sigma, sigma^2 was the peak value og the plot
+sigma= sqrt(0.001484); 
+
+%Finding lamda 
+lambda = 0.07;
+Kw = 2*lambda*w_0*sigma;
+
+ss = (Kw^2*omega.^2)./(omega.^4+w_0^4 +2*omega.^2*w_0^2*(-1+2*lambda^2));
+plot(omega,ss);
+legend('pxx','P with chosen lambda');
